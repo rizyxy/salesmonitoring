e4bd6060603511e7ce2d599220d6e919a2d79a75
Mukhammad Rizki <rizkyRE7@gmail.com>
2023-10-03 03:09:37 +0700

    Added Add Product Page

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.all_products, name='products')
+    path('products/', views.all_products, name='products'),
+    path('/products/add-product', views.add_product, name='add-product')
 ]
