ed2ae95ad8aa471febc56fe24f68bfbdd62acd85
Mukhammad Rizki <rizkyRE7@gmail.com>
2023-10-05 13:34:13 +0700

    Added Events App

---
+++ events/views.py
@@ -0,0 +1,6 @@
+from django.template import loader
+from django.http import HttpResponse
+
+def all_events(request):
+    template = loader.get_template('all_events.html')
+    return HttpResponse(template.render(None, request))
