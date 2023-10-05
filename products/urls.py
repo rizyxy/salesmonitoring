28b7034fe72d74adb74bd3eda718c4bde8fec208
Mukhammad Rizki <rizkyRE7@gmail.com>
2023-10-05 13:42:20 +0700

    Fixed Url Namespace

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('products/', views.all_products, name='products'),
+    path('products/', views.all_products, name='all-products'),
     path('/products/add-product', views.add_product, name='add-product')
 ]
--- shops/urls.py
+++ shops/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('shops/', views.all_shops, name='shops'),
+    path('shops/', views.all_shops, name='all-shops'),
     path('shops/add-shop', views.add_shop, name='add-shop')
 ]
