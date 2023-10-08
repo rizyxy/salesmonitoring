17d5bb9692c9f9803c91f17a7d38454598932df4
Salma <salmaaja150602@gmail.com>
2023-10-08 13:50:45 +0700

    menambah add event

--- events/urls.py
+++ events/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('events/', views.all_events, name='all-events')
+    path('events/', views.all_events, name='all-events'),
+    path('/events/add-events', views.add_events, name='add-events')
 ]
--- events/views.py
+++ events/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def all_events(request):
     template = loader.get_template('all_events.html')
     return HttpResponse(template.render(None, request))
+def add_events(request):
+    template = loader.get_template('add_events.html')
+    return HttpResponse(template.render(None, request))
